5b7f4026036c558df0ee48feb23734016760d5b5
Dawson-Gulasa <gulasadawson@yahoo.com>
2025-12-01 16:29:09 -0500

    Add files via upload

---
+++ reaction_time_training_button/firmware/config/secrets_template.py
@@ -0,0 +1,7 @@
+# secrets.py  (store on the Pico)
+WIFI_SSID = "[WIFI NAME]"
+WIFI_PASS = "[WIFI PASSWORD]"
+TS_WRITE_KEY = "[ThingSpeak Write Key]"
+TS_READ_KEY  = "[ThingSpeak Read Key]"
+TS_CHANNEL_ID = "[ThingSpeak Channel ID]" 
+
